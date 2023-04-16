276c23702be7d8ab4abb90e3147c92f5f0ab4b9d
Ibrahim Onuralp Yigit <onuralpyigit@gmail.com>
2023-04-16 21:31:04 +0000

    Add data preprocessor to parse codemr data files

--- input/java-design-patterns-master/model-view-intent/src/main/java/com/iluwatar/model/view/intent/actions/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Handle actions for {@link com.iluwatar.model.view.intent.CalculatorModel}
- * defined by {@link com.iluwatar.model.view.intent.actions.CalculatorAction}.
- */
-
-package com.iluwatar.model.view.intent.actions;
--- input/java-design-patterns-master/model-view-intent/src/main/java/com/iluwatar/model/view/intent/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Define Model, View and ViewModel.
- * Use them in {@link com.iluwatar.model.view.intent.App}
- */
-
-package com.iluwatar.model.view.intent;
